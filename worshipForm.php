ed0aa5b3471b5deae894dadc4fe0bcdbc85cc008
David <tommyyeh3210@gmail.com>
2016-04-27 14:28:49 +0800

    修改頁面title

--- worshipForm.php
+++ worshipForm.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Calendar</title>
+    <title>內湖思恩堂內部網站-敬拜表單</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
